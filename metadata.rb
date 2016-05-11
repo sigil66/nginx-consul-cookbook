da10fbfd618e4a698a4ee5add9fee17d6e7f48b4
Zachary Schneider <sigil.66@gmail.com>
2016-05-11 10:20:24 -0500

    lower consul refresh

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ops@sigil66.com'
 license          'Apache 2.0'
 description      'Installs/Configures nginx-consul'
 long_description 'Installs/Configures nginx-consul'
-version          '0.1.5'
+version          '0.1.6'
 
 depends          'sigil66'
