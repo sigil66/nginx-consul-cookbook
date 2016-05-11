faaf21060210e3340da079047cd9e9828d3ea143
Zachary Schneider <sigil.66@gmail.com>
2016-05-11 10:02:22 -0500

    add vagrant recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,7 @@
 #
 
 include_recipe "sigil66::default"
+include_recipe "sigil66::vagrant"
 include_recipe "nginx-consul::consul"
 include_recipe "nginx-consul::nginx"
 include_recipe "nginx-consul::vertx"
